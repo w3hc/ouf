e108c6c141208270d161a7a9c40e92a85d63e57f
julienbrg <julien@strat.cc>
2025-02-04 10:35:42 +0100

    rename

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Genji',
-  description: 'Next.js + Web3 Modal + Ethers.js + Chakra UI',
+  title: 'Ouf',
+  description: 'Our friendly assistant',
 }
